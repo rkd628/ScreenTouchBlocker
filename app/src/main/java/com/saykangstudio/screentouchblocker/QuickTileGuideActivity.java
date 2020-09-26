0f1dabde528d69510b837e7efa6ff34e439ef86a
seokil <rkd628@gmail.com>
2020-09-27 02:30:48 +0900

    Implement GUI guide activity

--- app/src/main/java/com/saykangstudio/screentouchblocker/QuickTileGuideActivity.java
+++ app/src/main/java/com/saykangstudio/screentouchblocker/QuickTileGuideActivity.java
@@ -5,8 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.drawable.AnimatedImageDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
@@ -17,6 +19,12 @@ public class QuickTileGuideActivity extends Activity {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, "onReceive intent = " + intent.getStringExtra("message"));
+
+            String message = intent.getStringExtra("message");
+            if (message.equals("ADDED")) {
+                QuickTileGuideActivity.this.setResult(Activity.RESULT_OK);
+                QuickTileGuideActivity.this.finish();
+            }
         }
     };
 
@@ -25,9 +33,11 @@ public class QuickTileGuideActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quick_tile_guide_layout);
 
+        final ImageView imageView = findViewById(R.id.GuideImage);
+        AnimatedImageDrawable drawable = (AnimatedImageDrawable) imageView.getDrawable();
+        drawable.start();
         LocalBroadcastManager.getInstance(this).registerReceiver(
                 mTileServiceBroadcastReceiver, new IntentFilter(Utils.ACTION_QUICK_TILE_SETTING));
-
     }
 
     @Override
