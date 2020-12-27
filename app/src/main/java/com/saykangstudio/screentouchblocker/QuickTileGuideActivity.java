f51fbbcf55015bac13e6cdf68545ce729556dbb1
seokil <rkd628@gmail.com>
2020-12-27 23:28:23 +0900

    Working on scroll..

--- app/src/main/java/com/saykangstudio/screentouchblocker/QuickTileGuideActivity.java
+++ app/src/main/java/com/saykangstudio/screentouchblocker/QuickTileGuideActivity.java
@@ -1,5 +1,6 @@
 package com.saykangstudio.screentouchblocker;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,7 +9,11 @@ import android.content.IntentFilter;
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
@@ -40,6 +45,46 @@ public class QuickTileGuideActivity extends Activity {
         final ImageView imageView1 = findViewById(R.id.GuideImage1);
         AnimatedImageDrawable drawable1 = (AnimatedImageDrawable) imageView1.getDrawable();
         drawable1.start();
+
+        final ImageView imageView2 = findViewById(R.id.GuideImage2);
+        AnimatedImageDrawable drawable2 = (AnimatedImageDrawable) imageView2.getDrawable();
+        drawable2.start();
+
+        final HorizontalScrollView scrollView = findViewById(R.id.QuickTileGuideHorizontalScrollView);
+        scrollView.post(new Runnable() {
+            @Override public void run() {
+
+                scrollView.fullScroll(ScrollView.FOCUS_RIGHT);
+//                scrollView.smoothScrollTo(500, 100);
+            }
+        });
+
+        scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//                Log.d(TAG, "onScrollChange v = " + v + ", scrollX = " + scrollX
+//                        + ", scrollY = " + scrollY + ", oldScrollX = "
+//                        + oldScrollX + ", oldScrollY = " + oldScrollY);
+            }
+        });
+
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d(TAG, "ACTION_DOWN = " + v.getScrollX());
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d(TAG, "ACTION_UP = " + v.getScrollX());
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+
         LocalBroadcastManager.getInstance(this).registerReceiver(
                 mTileServiceBroadcastReceiver, new IntentFilter(Utils.ACTION_QUICK_TILE_SETTING));
     }
